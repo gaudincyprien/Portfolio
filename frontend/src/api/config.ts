13459ac5fa912dfee624c02aa80b9fa916826624
gaudincyprien <cyprien.gaudin@gmail.com>
2024-02-17 17:13:04 +0100

    change config

--- frontend/src/api/config.ts
+++ frontend/src/api/config.ts
@@ -8,7 +8,7 @@ export async function get_raquettes(nb_joueurs: number, terrain: string[]) {
         };
     
         return await axios.post(
-            "http://cypriengaudin.fr/tennis/afterwork/new_matchs",
+            "https://cypriengaudin.fr/tennis/afterwork/new_matchs",
             data,
             { headers: { 'Content-Type': 'application/json' } }
         ) as AxiosResponse<ConfigAPIRaquettes>;
